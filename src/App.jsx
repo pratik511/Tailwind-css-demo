af6272c5c5b041f4c38d1dd360f4ec15d988d6ee
Pratik <pp05111999@gmail.com>
2024-03-07 15:19:03 +0530

    env add

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,47 @@ import "./App.css";
 import router from "./routes";
 
 function App() {
+  document.addEventListener('visibilitychange', function () {
+    if (document.hidden) {
+      console.log('not visible');
+    } else {
+      console.log('is visible');
+    }
+  });
+
+  window.addEventListener('focus', function () {
+    console.log('has focus');
+  });
+
+  window.addEventListener('blur', function () {
+    console.log('lost focus');
+  });
+
+  if (import.meta.env.VITE_APP_EVENT_STATUS) {
+    document.onkeydown = (e) => {
+      if (e.key == "F12") {
+        e.preventDefault();
+      }
+      if (e.ctrlKey && e.shiftKey && e.code == 'KeyI') {
+        e.preventDefault();
+      }
+      if (e.ctrlKey && e.shiftKey && e.code == 'KeyC') {
+        e.preventDefault();
+      }
+      if (e.ctrlKey && e.shiftKey && e.code == 'KeyJ') {
+        e.preventDefault();
+      }
+      if (e.ctrlKey && e.code == 'KeyU') {
+        e.preventDefault();
+      }
+    };
+
+    document.addEventListener('contextmenu', function (e) {
+      e.preventDefault();
+    });
+
+    document.onselectstart = new Function("return false")
+  }
 
   return (
     <>
